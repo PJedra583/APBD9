0c8a66b63a2f2fe6b0ed7f9e9b2d4f0447692ed5
29010 <s29010@pjwstk.edu.pl>
2024-05-28 13:20:51 +0200

    MyCommit

---
+++ APBD9/Controllers/TripsController.cs
@@ -0,0 +1,34 @@
+﻿using APBD9.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBD9.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips.ToListAsync();
+
+        var tripsInclude = await _context.Trips.Include(e => e.IdCountries)
+            .ToListAsync();
+        return Ok(tripsInclude.Select(e => new
+            {
+                Name = e.Name,
+                Countries = e.IdCountries.Select(c => new
+                {
+                    Name = c.Name
+                })
+            })
+        );
+    }
+}
